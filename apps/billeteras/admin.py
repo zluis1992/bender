811247dd95134741094cf658eeec12932d729cb6
zluis1992 <zluissigloxx@hotmail.com>
2017-07-09 21:53:54 -0500

    primer commit
    subida de archivos al repositorio

---
+++ apps/billeteras/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from apps.billeteras.models import Billetera
+# Register your models here.
+admin.site.register(Billetera)
---
+++ apps/billeteras/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Billetera(models.Model):
+	moneda = models.CharField(max_length=50, unique = True)
+	porcentaje_retiro = models.DecimalField( max_digits=3, decimal_places=2)
+	cantidad = models.DecimalField(max_digits=13, decimal_places=5)
