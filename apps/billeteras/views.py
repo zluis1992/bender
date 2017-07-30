66a733c3a940df46e602ee36d8fb42160dfe4236
zluis1992 <zluissigloxx@hotmail.com>
2017-07-29 21:51:58 -0500

    creacion de modulo poloniex
    cree un modulo que llame poloniex dentro de apps/billeteras/ para que aca esten recolectadas todas las funciones del api de poloniex y se puedan utilizar en las vistas que tengamos

--- apps/billeteras/views.py
+++ apps/billeteras/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apps.billeteras.poloniex import poloniex
 
 # Create your views here.
 
+
+#valor= raw_input('amo voy a traerte todo el historial de trading de un moneda... por favor no me grites y dime cual par quieres?: ')
+
+prueba = poloniex('PBU6QU8V-CET88BVI-VOOIHFDT-5CNMC7PR','34ebbae8bf11a4c759d606ce86674eac901f1f6293c14a1014abf437b33cb14e2a3a535fa84e98e83415e4870a49cb0e505cac1dbfdd8b98f2be090441021681c')
+#print prueba.bar()
+ticker = prueba.returnTicker()
+valorEth = float(ticker['BTC_ETH']['last'])
+
+#sw = 0
+#while (sw == 0):
+	#ticker = prueba.returnTicker()
+	#valorEth = float(ticker['BTC_ETH']['last'])
+	
+	#if 0.10240000 <= valorEth <= 0.10340000:
+	#	print ('Hey compra')
+		
+	#elif 0.11910000 <= valorEth <= 0.12100000:
+	#	print ('Hey vende')
+		
+		
+		
+
+#print "Tenemos "+str(decoded["BTC_BCN"][0]["ETH"][0])+" Lechugas."
+
+
 def index(request):
-	return HttpResponse("Index")
+	return HttpResponse(valorEth)
