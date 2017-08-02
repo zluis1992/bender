e809f69958e3838b001f5f8d919cc42a79250588
zluis1992 <zluissigloxx@hotmail.com>
2017-08-01 22:55:50 -0500

    creacion de plantillas
    cree ya la plantilla y coloque el css para que se viera bonito

--- apps/billeteras/views.py
+++ apps/billeteras/views.py
@@ -6,8 +6,8 @@ from .poloniex import poloniex
 
 
 # Create your views here.
-prueba = poloniex('PBU6QU8V-CET88BVI-VOOIHFDT-5CNMC7PR','34ebbae8bf11a4c759d606ce86674eac901f1f6293c14a1014abf437b33cb14e2a3a535fa84e98e83415e4870a49cb0e505cac1dbfdd8b98f2be090441021681c')
-#print prueba.bar()
+prueba = poloniex('PBU6QU8V-CET88BVI-VOOIHFDT-5CNMC7PR',  '34ebbae8bf11a4c759d606ce86674eac901f1f6293c14a1014abf437b33cb14e2a3a535fa84e98e83415e4870a49cb0e505cac1dbfdd8b98f2be090441021681c')
+# print prueba.bar()
 ticker = prueba.returnTicker()
 valorEth = float(ticker['BTC_ETH']['last'])
 
@@ -21,12 +21,8 @@ valorEth = float(ticker['BTC_ETH']['last'])
 		
 	#elif 0.11910000 <= valorEth <= 0.12100000:
 	#	print ('Hey vende')
-		
-		
-		
-
-#print "Tenemos "+str(decoded["BTC_BCN"][0]["ETH"][0])+" Lechugas."
 
 
 def index(request):
-	return HttpResponse(valorEth)
+	#return HttpResponse(valorEth)
+	return render(request, 'billeteras/index.html')
